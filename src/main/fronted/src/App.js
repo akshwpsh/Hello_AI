e53b2f95fd31ff9231da4e49f11d85a37ab53aaf
gusal <akshwpsh@gmail.com>
2024-05-31 02:34:23 +0900

    노트북 재 업로드

---
+++ src/main/fronted/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Chatbot from "./Chatbot";
+import Chatbot_Gemini from "./Chatbot_Gemini";
+
+function App() {
+  return (
+    <div className="App">
+      <Chatbot_Gemini />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/test/java/com/gusal/hello_ai/HelloAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gusal.hello_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
